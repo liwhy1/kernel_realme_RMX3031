ca0bca9144df3bf62dce2d84ed5abd4945252067
liwhy1 <levi.toth2005@gmail.com>
2026-01-20 13:14:35 +0100

    build: Update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-clear
-function compile() 
-{
-echo ORIGIN KERNEL FOR REALME X7 MAX 5G, CODENAMED - CUPIDA
-echo
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G >/dev/null
@@ -18,21 +13,6 @@ if ! [ -a $gcc64bin ]; then git clone --depth=1 https://github.com/LineageOS/and
 fi
 gcc32bin=los-4.9-32/bin/arm-linux-androideabi-as
 if ! [ -a $gcc32bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
-fi
-read -p "Wanna do dirty build? (Y/N): " build_type
-if [[ $build_type == "N" || $build_type == "n" ]]; then
-echo Deleting out directory and doing clean Build
-rm -rf out && mkdir -p out
-fi
-if [[ $build_type == "Y" || $build_type == "y" ]]; then
-echo Warning :- Doing dirty build
-fi
-if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
-if ! [[ $build_type == "N" || $build_type == "n" ]]; then
-echo Invalid Input , Read carefully before typing
-echo Trying to restart script
-. build.sh && exit
-fi
 fi
 
 make O=out ARCH=arm64 cupida_defconfig
@@ -51,10 +31,7 @@ make -j$(nproc --all)   O=out \
                         LLVM_IAS=1 \
                         OBJCOPY=llvm-objcopy \
                         CONFIG_NO_ERROR_ON_MISMATCH=y
-}
 
-function zupload()
-{
 zimage=out/arch/arm64/boot/Image.gz-dtb
 if ! [ -a $zimage ];
 then
@@ -62,17 +39,10 @@ echo  " Failed to compile zImage, fix the errors first "
 else
 echo -e " Build succesful, generating flashable zip now "
 anykernelbin=AnyKernel/anykernel.sh
-if ! [ -a $anykernelbin ]; then git clone --depth=1 https://github.com/nishant6342/AnyKernel3 -b cupida  AnyKernel
+if ! [ -a $anykernelbin ]; then git clone --depth=1 https://github.com/liwhy1/AnyKernel3 -b cupida AnyKernel
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 ORIGIN-OSS-KERNEL-RMX3031.zip *
-#curl --upload-file ORIGIN-OSS-KERNEL-RMX3031.zip https://transfer.sh/
-curl -sL https://git.io/file-transfer | sh
-./transfer anon ORIGIN-OSS-KERNEL-RMX3031.zip
 cd ../
 fi
-}
-
-compile
-zupload
